833188587773e9994980c63a6fcd291d133a869f
NotePrin <kunprin.note@gmail.com>
2024-09-20 09:07:27 +0700

    add tab menu

--- src/pages/assetDetails/assetDetails.tsx
+++ src/pages/assetDetails/assetDetails.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { mockAssetData } from "./__mock__/mockAsset";
 import { TAssetData } from "./types";
 import LandingHeader from "@/layout/landing/landingHeader";
@@ -13,6 +13,7 @@ export function AssetDetails() {
   const assetId = useParams().id;
   console.log(assetId);
   const [assetData, setAssetData] = useState<TAssetData | undefined>(undefined);
+  const [tab, setTab] = useState(0);
 
   const fetchAssetData = async () => {
     setAssetData(mockAssetData);
@@ -103,7 +104,84 @@ export function AssetDetails() {
               <div className="h-20 w-full px-6 border-b border-gray-300"></div>
             </CardHeader>
             <CardContent>
-              <div className="w-full h-[700px] px-6 py-6"></div>
+              {/* <div className="w-full h-[700px] px-6 py-6"></div> */}
+              <div className="box-body">
+                <div className="border-b-2 border-gray-200 dark:border-white/10">
+                  <nav className="-mb-0.5 flex space-x-6 rtl:space-x-reverse">
+                    <Link
+                      className={`py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 dark:text-white/70 hover:text-primary ${
+                        tab === 1
+                          ? "hs-tab-active:font-semibold hs-tab-active:border-primary hs-tab-active:text-primary active"
+                          : ""
+                      }`}
+                      // className="hs-tab-active:font-semibold hs-tab-active:border-primary hs-tab-active:text-primary py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm whitespace-nowrap text-gray-500  dark:text-white/70 hover:text-primary active"
+                      to="#"
+                      id="underline-item-1"
+                      data-hs-tab="#underline-1"
+                      aria-controls="underline-1"
+                      onClick={() => setTab(1)}
+                    >
+                      Tab 1
+                    </Link>
+                    <Link
+                      className={`py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 dark:text-white/70 hover:text-primary ${
+                        tab === 2
+                          ? "hs-tab-active:font-semibold hs-tab-active:border-primary hs-tab-active:text-primary active"
+                          : ""
+                      }`}
+                      // className="hs-tab-active:font-semibold hs-tab-active:border-primary hs-tab-active:text-primary py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm whitespace-nowrap text-gray-500  dark:text-white/70 hover:text-primary"
+                      to="#"
+                      id="underline-item-2"
+                      data-hs-tab="#underline-2"
+                      aria-controls="underline-2"
+                      onClick={() => setTab(2)}
+                    >
+                      Tab 2
+                    </Link>
+                  </nav>
+                </div>
+
+                <div className="mt-3">
+                  {tab === 1 && (
+                    <div
+                      id="underline-1"
+                      role="tabpanel"
+                      aria-labelledby="underline-item-1"
+                    >
+                      <p className="text-gray-500 dark:text-white/70 p-5 border rounded-sm dark:border-white/10 border-gray-200">
+                        How hotel deals can help you live a better life. How
+                        celebrity cruises aren't as bad as you think. How
+                        cultural solutions can help you predict the future. How
+                        to cheat at dog friendly hotels and get away with it. 17
+                        problems with summer activities. How to cheat at travel
+                        agents and get away with it. How not knowing family trip
+                        ideas makes you a rookie. What everyone is saying about
+                        daily deals. How twitter can teach you about carnival
+                        cruises. How to start using cultural solutions.
+                      </p>
+                    </div>
+                  )}
+                  {tab === 2 && (
+                    <div
+                      id="underline-2"
+                      role="tabpanel"
+                      aria-labelledby="underline-item-2"
+                    >
+                      <p className="text-gray-500 dark:text-white/70 p-5 border rounded-sm dark:border-white/10 border-gray-200">
+                        How travel coupons make you a better lover. Why cultural
+                        solutions are the new black. Why mom was right about
+                        travel insurances. How family trip ideas can help you
+                        predict the future. How carnival cruises make you a
+                        better lover. Why you'll never succeed at daily deals.
+                        11 ways cheapest flights can find you the love of your
+                        life. The complete beginner's guide to mission trips. If
+                        you read one article about cultural notes read this one.
+                        Why you shouldn't eat vacation package in bed.
+                      </p>
+                    </div>
+                  )}
+                </div>
+              </div>
             </CardContent>
           </Card>
         </div>
