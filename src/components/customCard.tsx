6c689c28d1a0b93a1eef634dd593bc2900820bba
NotePrin <kunprin.note@gmail.com>
2024-09-23 12:36:49 +0700

    merge prin2

--- src/components/customCard.tsx
+++ src/components/customCard.tsx
@@ -13,7 +13,7 @@ export function CustomCard({ data }: TCustomCardProps) {
       <div className="w-[360px] h-[500px] px-[10px] py-[10px] flex flex-col justify-evenly">
         <div className="w-full p-4 space-y-4">
           <div className="flex flex-row items-center space-x-4">
-            <img src={data.logo} alt="" className="h-[24px]" />
+            <img src={data.logo} alt="" className="h-[34px]" />
             <h1 className={`text-xl font-bold ${darkText}`}>
               Elite Consulting
             </h1>
