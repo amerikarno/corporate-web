77e1accb27981128205971aa186adb7bc8a3a7d7
NotePrin <kunprin.note@gmail.com>
2024-09-26 15:03:52 +0700

    remove screen tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "1280px",
-      // => @media (min-width: 1024px) { ... }
-
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
-    },
     extend: {
       animation: {
         fade: "fadeIn .5s ease-in-out",
