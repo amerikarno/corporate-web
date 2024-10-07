5bea27c9f459a736b77a0a45ce75eb64325083eb
NotePrin <kunprin.note@gmail.com>
2024-10-07 16:07:35 +0700

    order invest, clean code, connect api create invest

--- src/pages/orderTrade/constant/schemas.ts
+++ src/pages/orderTrade/constant/schemas.ts
@@ -2,11 +2,11 @@ import { z } from "zod";
 
 export const orderTradeSchema = z.object({
   id: z.string().optional(),
-  customerCode: z.string(),
-  icoCode: z.string(),
-  amount: z.string().min(1, "required"),
-  value: z.string().min(1, "required"),
-  currency: z.string().min(1, "required"),
+  customerCode: z.string().optional(),
+  icoCode: z.string().optional(),
+  amount: z.string().min(1, "required amount"),
+  value: z.string().min(1, "required value"),
+  currency: z.string().min(1, "required currency"),
   status: z.number().optional(),
 });
 
--- src/pages/orderTrade/orderTrade.tsx
+++ src/pages/orderTrade/orderTrade.tsx
@@ -3,16 +3,19 @@ import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 // import { TOrderTrade } from "./constant/type";
 import { orderTradeSchema, TOrderTrade } from "./constant/schemas";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React, { useEffect, useState } from "react";
 import DataTable, { TableColumn } from "react-data-table-component";
 import { MdCurrencyExchange } from "react-icons/md";
 import { IoReceiptOutline } from "react-icons/io5";
 import NavBar from "@/components/navbar";
-import { consoleLog, sleep } from "@/lib/utils";
+import { consoleLog, getUser, sleep } from "@/lib/utils";
 import { TAssetData } from "../assetDetails/types";
 import { mockAssetData } from "../assetDetails/__mock__/mockAsset";
+import axios from "@/api/axios";
+import { getCookies } from "@/lib/cookies";
+import { TUser } from "../authentication/login/types";
 
 type TCurrency = {
   name: string;
@@ -20,23 +23,25 @@ type TCurrency = {
 };
 
 export default function OrderTrade() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+    reset,
+    setValue,
+    trigger,
+  } = useForm<TOrderTrade>({
+    resolver: zodResolver(orderTradeSchema),
+    mode: "onChange",
+  });
+
   const [selectedCurrency, setSelectedCurrency] = useState<TCurrency>({
     name: "THB",
     factor: 1,
   });
-  // const [tokenAmount, setTokenAmount] = useState<string>("");
-  // const [currencyAmount, setCurrencyAmount] = useState<string>("");
+  const [user, setUser] = useState<TUser | undefined>();
   const [assetData, setAssetData] = useState<TAssetData | undefined>();
-  const [lot, setLot] = useState<number>(0);
   const [unitPrice, setUnitPrice] = useState<number>(1);
-  const [tradeData, setTradeData] = useState<TOrderTrade>({
-    customerCode: "",
-    icoCode: "",
-    amount: "",
-    currency: "",
-    value: "",
-  });
-  // const [choosedEditData, setChoosedEditData] = useState<TOrderTrade>();
   const payCurrency = [
     // factor is from xx curency = 1 thb. eg 35 usd= 1 thb
     { name: "", factor: 1 },
@@ -50,10 +55,10 @@ export default function OrderTrade() {
     consoleLog(data);
   };
 
-  const handleTokenAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleTokenAmount = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     const inputValue = value.replace(/[^0-9.,]/g, "");
-    // setTokenAmount(inputValue);
+    setValue("amount", inputValue);
     const formattedValue = inputValue.replace(/,(?=\.)/g, "").replace(/,/g, "");
     const numValue = parseFloat(formattedValue);
     const numCurrencyAmount = (numValue * unitPrice) / selectedCurrency.factor;
@@ -61,18 +66,17 @@ export default function OrderTrade() {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     });
-    // setCurrencyAmount(formattedCurrencyAmount);
-    setTradeData({
-      ...tradeData,
-      value: formattedCurrencyAmount,
-      amount: inputValue,
-    });
+    setValue("value", formattedCurrencyAmount);
+    await trigger("amount");
+    await trigger("value");
   };
 
-  const handleCurrencyAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleCurrencyAmount = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const { value } = e.target;
     const inputValue = value.replace(/[^0-9.,]/g, "");
-    // setCurrencyAmount(inputValue);
+    setValue("value", inputValue);
     const formattedValue = inputValue.replace(/,(?=\.)/g, "").replace(/,/g, "");
     const numValue = parseFloat(formattedValue);
     const numTokenAmount = (numValue * selectedCurrency.factor) / unitPrice;
@@ -80,22 +84,14 @@ export default function OrderTrade() {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     });
-    // setTokenAmount(formattedTokenAmount);
-    setTradeData({
-      ...tradeData,
-      value: inputValue,
-      amount: formattedTokenAmount,
-    });
+    setValue("amount", formattedTokenAmount);
+    await trigger("amount");
+    await trigger("value");
   };
 
   const fetchCorporateCodes = async () => {
     await sleep(1000);
     setAssetData(mockAssetData);
-    const lot = mockAssetData.info!.minimumInvestmentQuantity.split(" ")[0];
-    const numLot = parseFloat(lot);
-    if (!isNaN(numLot)) {
-      setLot(numLot);
-    }
     const unitPrice = mockAssetData.info!.issueUnitPrice.split(" ")[0];
     const numUnitPrice = parseFloat(unitPrice);
     if (!isNaN(numUnitPrice)) {
@@ -103,37 +99,7 @@ export default function OrderTrade() {
     }
   };
 
-  const fetchOrderList = async () => {
-    // try {
-    //   const token = getCookies();
-    //   const res = await axios.get("/api/v1/transaction/order/get", {
-    //     headers: {
-    //       Authorization: `Bearer ${token}`,
-    //     },
-    //   });
-    //   if (res.status === 200) {
-    //     // consoleLog(res.data);
-    //     // const orderTrades = res.data || [];
-    //     // const uniqueOrderTrades = orderTrades.filter(
-    //     //   (order: any, index: any, self: any) =>
-    //     //     index === self.findIndex((t: any) => t.id === order.id)
-    //     // );
-    //     // const adjustedOrderTrades = uniqueOrderTrades.map(
-    //     //   (order: TOrderTrade) => ({
-    //     //     ...order,
-    //     //     cryptoAmount: (Number(order.cryptoAmount) / 100000).toString(),
-    //     //     cryptoPrice: (Number(order.cryptoPrice) / 100000).toString(),
-    //     //     fiatAmount: (Number(order.fiatAmount) / 100000).toString(),
-    //     //   })
-    //     // );
-    //     // consoleLog("OrderTrade data fetched successfully.", uniqueOrderTrades);
-    //   } else {
-    //     consoleLog("Failed to fetch orderTrade");
-    //   }
-    // } catch (error) {
-    //   consoleLog("Fetching order list of this role error!", error);
-    // }
-  };
+  const fetchOrderList = async () => {};
 
   const getStatus = (status?: number) => {
     if (status === -1) {
@@ -191,61 +157,16 @@ export default function OrderTrade() {
     },
   ];
 
-  // useEffect(() => {
-  //   const orderListDatatoInputField = choosedEditData || {
-  //     corporateCode: null,
-  //     cryptoAmount: null,
-  //     cryptoPrice: null,
-  //     currency: "",
-  //     fiatAmount: null,
-  //   };
-  //   reset(orderListDatatoInputField);
-  //   if (choosedEditData?.operations === "buy") {
-  //     setBuySell("buy");
-  //   } else if (choosedEditData?.operations === "sell") {
-  //     setBuySell("sell");
-  //     if (choosedEditData) {
-  //       const cur = choosedEditData.pair.split("/");
-  //       setSellCurrency(cur[1]);
-  //     }
-  //   } else {
-  //     setBuySell("buy");
-  //   }
-
-  //   consoleLog("use effect", orderListDatatoInputField);
-  // }, [choosedEditData]);
-
-  // const handleBuySell = (value: string) => {
-  //   setBuySell(value);
-  //   if (value === "sell") {
-  //     const cur = selectedTradingPair.split("/");
-  //     setSellCurrency(cur[1]);
-  //   }
-  // };
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-    reset,
-  } = useForm<TOrderTrade>({
-    resolver: zodResolver(orderTradeSchema),
-  });
-
   useEffect(() => {
     if (!assetData) {
+      const user = getUser();
+      setUser(user ? user : undefined);
       fetchCorporateCodes();
       fetchOrderList();
     }
   }, [reset]);
 
-  // const handleCorporateCodeChange = (
-  //   event: React.ChangeEvent<HTMLInputElement>
-  // ) => {
-  //   setSelectedCorporateCode(Number(event.target.value) || null);
-  // };
-
-  const handleCurrencyChange = (
+  const handleCurrencyChange = async (
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
     const currency = payCurrency.find(
@@ -253,29 +174,20 @@ export default function OrderTrade() {
     );
     if (currency) {
       setSelectedCurrency(currency);
-
+      const tokenUnit = document.getElementById("amount") as HTMLInputElement;
+      let token = "0";
+      if (tokenUnit) {
+        token = tokenUnit.value;
+      }
       const numCurrencyAmount =
-        (parseFloat(tradeData.amount) * unitPrice) / currency.factor;
-
-      // setCurrencyAmount(
-      //   numCurrencyAmount.toLocaleString("en-us", {
-      //     maximumFractionDigits: 2,
-      //     minimumFractionDigits: 2,
-      //   })
-      // );
-      setTradeData({
-        ...tradeData,
-        value: numCurrencyAmount.toLocaleString("en-us", {
-          maximumFractionDigits: 2,
+        (parseFloat(token) * unitPrice) / currency.factor;
+      setValue(
+        "value",
+        numCurrencyAmount.toLocaleString("en-us", {
           minimumFractionDigits: 2,
-        }),
-      });
-      // setTokenAmount(
-      //   numTokenAmount.toLocaleString("en-us", {
-      //     maximumFractionDigits: 2,
-      //     minimumFractionDigits: 2,
-      //   })
-      // );
+          maximumFractionDigits: 2,
+        })
+      );
     } else {
       setSelectedCurrency({
         name: "",
@@ -284,19 +196,32 @@ export default function OrderTrade() {
     }
   };
 
-  const onSubmit = async (data: TOrderTrade) => {
-    consoleLog(data);
-    if (parseFloat(data.amount) < lot) {
-      const error = {
-        issues: [
-          {
-            code: "custom",
-            message: `Amount must be greater than or equal to the minimum size of ${lot}.`,
-            path: ["amount"],
+  const onsubmit = async (data: TOrderTrade) => {
+    reset();
+    let body: TOrderTrade = {
+      ...data,
+      customerCode: user?.id,
+      icoCode: assetData?.info?.id,
+    };
+    consoleLog("data", body);
+    try {
+      const res = await axios.post(
+        "/api/v1/customer/product/investment",
+        body,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + getCookies(),
           },
-        ],
-      };
-      error.issues.push(...error.issues);
+        }
+      );
+      if (res.status === 200) {
+        consoleLog(res.data);
+      } else {
+        consoleLog(res.data);
+      }
+    } catch (error) {
+      consoleLog(error);
     }
   };
 
@@ -306,7 +231,7 @@ export default function OrderTrade() {
         children={
           <div className="w-full flex justify-center">
             <div className="w-full max-w-[1240px] md:px-4 flex flex-col justify-center space-y-4 py-10">
-              <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+              <form className="space-y-4" onSubmit={handleSubmit(onsubmit)}>
                 <div className="w-full flex justify-center">
                   <Card className="bg-white w-full p-4 md:space-y-4 md:p-10">
                     <span className="flex justify-start items-center font-bold md:text-xl py-2 gap-2">
@@ -322,6 +247,7 @@ export default function OrderTrade() {
                             <select
                               id="currency"
                               {...register("currency")}
+                              name="currency"
                               value={selectedCurrency.name}
                               onChange={handleCurrencyChange}
                               disabled={isSubmitting}
@@ -343,13 +269,17 @@ export default function OrderTrade() {
                         </div>
                         <div>
                           <Input
-                            {...(register("amount"),
-                            { onChange: handleTokenAmount })}
+                            {...register("amount", {
+                              // value: tokenAmount,
+                              onChange: handleTokenAmount,
+                            })}
                             label={`Token`}
                             data-testid="tokenAmount"
-                            id="tokenAmount"
                             disabled={isSubmitting}
-                            value={tradeData.amount}
+                            id="amount"
+                            name="amount"
+                            // value={tokenAmount}
+                            // onChange={handleTokenAmount}
                             inputClassName="text-right"
                           />
                           {errors.amount && (
@@ -361,13 +291,17 @@ export default function OrderTrade() {
 
                         <div>
                           <Input
-                            {...(register("value"),
-                            { onChange: handleCurrencyAmount })}
+                            {...register("value", {
+                              // value: currencyAmount,
+                              onChange: handleCurrencyAmount,
+                            })}
                             label={`Amount (${selectedCurrency.name})`}
                             data-testid="fiatValue"
-                            id="fiatValue"
                             disabled={isSubmitting}
-                            value={tradeData.value}
+                            id="value"
+                            name="value"
+                            // value={currencyAmount}
+                            // onChange={handleCurrencyAmount}
                             inputClassName="text-right"
                           />
                           {errors.value && (
@@ -379,7 +313,6 @@ export default function OrderTrade() {
                         <div className="w-full flex justify-center py-6">
                           <Button
                             type="submit"
-                            disabled={isSubmitting}
                             className="w-full bg-slate-700 hover:bg-slate-900"
                           >
                             {isSubmitting ? "Submitting..." : "Submit"}
