b9c7ae602ae9a396680102224aa857e884c7dcb7
NotePrin <kunprin.note@gmail.com>
2024-10-30 13:08:03 +0700

    fix api path

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    build: {
+        chunkSizeWarningLimit: 3500,
+    },
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+    },
+    server: {
+        host: true,
+    },
+});
